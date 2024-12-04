6dab59a97f66cfa95e970b609fb9b43eaa9f4ce7
Larry <peggydbc1217@gmail.com>
2024-12-04 21:31:09 +0800

    firest commit create starter file

---
+++ app/cabins/page.js
@@ -0,0 +1,17 @@
+import Counter from "@/app/_components/Counter";
+
+export default async function Page() {
+  return (
+    <div>
+      <h1>Cabins page</h1>
+
+      <ul>
+        {data.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+
+      <Counter users={data} />
+    </div>
+  );
+}
---
+++ app/layout.js
@@ -0,0 +1,23 @@
+import Logo from "@/app/_components/Logo";
+import Navigation from "@/app/_components/Navigation";
+
+import "@/app/_styles/globals.css";
+
+export const metadata = {
+  title: "The Wild Oasis",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className="bg-blue-900 text-gray-50 min-h-screen">
+        <header>
+          <Logo />
+          <Navigation />
+        </header>
+        <main>{children}</main>
+        <footer>Copyright by The Wild Oasis</footer>
+      </body>
+    </html>
+  );
+}
