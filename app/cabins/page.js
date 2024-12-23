fd0334e4359eed264d117f88f457449284665162
Larry <peggydbc1217@gmail.com>
2024-12-23 22:15:22 +0800

    feat/add loading state in cabins and supbase api

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,11 +1,13 @@
 import CabinCard from "@/app/_components/CabinCard";
+import { getCabins } from "@/app/_lib/data-service";
 
 export const metadata = {
   title: "Cabins",
 };
 
 export default async function Page() {
-  const cabins = [];
+  const cabins = await getCabins();
+  console.log(cabins);
 
   return (
     <div>
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: "pmmklsypwouryxzxzjmz.supabase.co",
+        port: "",
+        protocol: "https",
+        pathname: "/storage/v1/object/public/cabin-images/**",
+      },
+    ],
+  },
+};
 export default nextConfig;
